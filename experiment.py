f9967121b4952009c79ba3c48b5466c6dc792228
simaex.unknown <simaex.unknown@googlemail.com>
2024-08-16 13:29:02 +0000

    - Added function comments to the Experiment class - Moved the specification of the seed into the generate and decide methods of an Experiment so that they can be quickly changed with every method call. Removed temperature entirely as it is not yet fully supported by the rest of the code - Start tracking before the beginning of the run so that runtimes are measured appropriately

--- experiment.py
+++ experiment.py
@@ -24,21 +24,22 @@ class Experiment:
 
     Attributes:
         bias (str): The name of the cognitive bias tested in this experiment.
-        seed (int): The seed used for this experiment.
-        temperature (float): The temperature used for this experiment.
-        shuffle_answer_options (bool): Whether the answer options should be randomly shuffled when making a decision.
     """
 
-    def __init__(self, bias: str, seed: int = 42, temperature: float = 0.7, shuffle_answer_options: bool = False):
+    def __init__(self, bias: str):
         if not "WANDB_KEY" in os.environ:
             raise ValueError("Cannot track experiments due to missing Weights & Biases API key. Please store your API key in environment variable 'WANDB_KEY'.")
 
         self.bias = bias
-        self.seed = seed
-        self.temperature = temperature      # TODO Temperature is ignored so far
-        self.shuffle_answer_options = shuffle_answer_options
 
-    def get_random_scenario(self):
+    def get_random_scenario(self) -> str:
+        """
+        Samples a random scenario.
+
+        Returns:
+            str: The sample scenario string.
+        """
+
         # Load the pre-defined scenario strings
         with open("scenarios.txt") as f:
             scenarios = f.readlines()
@@ -51,9 +52,24 @@ class Experiment:
 
         return scenario
 
-    def generate(self, scenario: str) -> TestCase:
+    def generate(self, scenario: str, seed: int = 42) -> TestCase:
+        """
+        Generates a new test case for the specified scenario.
+
+        Args:
+            scenario (str): The scenario for which to generate the test case.
+            seed (int): The seed for generating the scenario.
+
+        Returns:
+            TestCase: The generated test case.
+        """
+
         # Load the test generator for the bias
         generator = get_generator(self.bias)
+
+        # Start tracking of this run
+        run_name = f"Generation {generator.BIAS}"
+        self._start(run_name)
 
         # Instantiate the population LLM
         population_model = GptFourO()       # TODO Allow selection of different LLMs, e.g., via passing the LLM's name as string and having a get_model() method
@@ -61,39 +77,62 @@ class Experiment:
         # TODO Maybe we can even log the raw LLM inputs and outputs. Can we have an internal prompt log inside the LLM class?
 
         # Generate a test case
-        test_case = generator.generate_all(population_model, [scenario], self.seed)[0]    # TODO Make sure this only returns one test case, e.g., by passing a list with a single variant to generate_all
+        test_case = generator.generate_all(population_model, [scenario], seed)[0]    # TODO Make sure this only returns one test case, e.g., by passing a list with a single variant to generate_all
 
-        # Track the run on Weights & Biases
-        run_name = f"Generation {test_case.BIAS}"
-        self._start(run_name)
+        # Track the run results and finish the run
         logs = self._create_test_case_logs(test_case)
         self._log(logs)
         self._finish()
 
         return test_case
 
-    def decide(self, test_case: TestCase) -> DecisionResult:
+    def decide(self, test_case: TestCase, seed: int = 42, shuffle_answer_options: bool = False) -> tuple[DecisionResult, float]:
+        """
+        Obtains a decision for a test case and calculates the biasedness.
+
+        Args:
+            test_case (TestCase): The test case defining the decision task.
+            seed (int): The seed to be used for obtaining the decision.
+            shuffle_answer_options (bool): If True, answer options in the test case will be randomly shuffled.
+
+        Returns:
+            tuple[DecisionResult, float]: A tuple containing the decision result and the calculated biasedness.
+        """
+
+        # Start tracking of this run
+        run_name = f"Decision {test_case.BIAS}"
+        self._start(run_name)
+
         # Load the metric for the bias
-        metric = get_metric(self.bias)
-        
+        metric = get_metric(self.bias)        
+
         # Instantiate the decision LLM
-        decision_model = GptThreePointFiveTurbo(shuffle_answer_options=self.shuffle_answer_options)
+        decision_model = GptThreePointFiveTurbo(shuffle_answer_options=shuffle_answer_options)
 
         # Obtain a decision for the test case
-        decision_result = decision_model.decide(test_case=test_case, temperature=self.temperature, seed=self.seed)
+        decision_result = decision_model.decide(test_case=test_case, seed=seed)
 
         # Calculate the biasedness
         biasedness = metric.compute([(test_case, decision_result)])
 
-        # Track the run on Weights & Biases
-        run_name = f"Decision {test_case.BIAS}"
-        self._start(run_name)
-        logs = self._create_decision_logs(test_case=test_case, decision_result=decision_result, biasedness=biasedness)
+        # Track the run results and finish the run
+        logs = self._create_decision_logs(test_case=test_case, decision_result=decision_result, biasedness=biasedness, shuffled_answer_options=shuffle_answer_options)
         self._log(logs)
         self._finish()
 
+        return decision_result, biasedness
+
     def _create_test_case_logs(self, test_case: TestCase) -> dict:
-        # TODO Maybe the TestCase class should have a to_dict() function that returns a dictionary representation of the test case
+        """
+        Function to be used after the generation of a test case. Converts a TestCase object into run logs.
+
+        Args:
+            test_case (TestCase): The TestCase that was generated during the run.
+
+        Returns:
+            dict: A dictionary of run logs.
+        """
+
         # TODO Temperature and seed should be stored inside the TestCase object for later reference
         logs = {
             "Type": "GENERATION",
@@ -110,8 +149,19 @@ class Experiment:
 
         return logs
 
-    def _create_decision_logs(self, test_case: TestCase, decision_result: DecisionResult, biasedness: float) -> dict:
-        # TODO Maybe the DecisionResult class should have a to_dict() function that returns a dictionary representation of the decision result
+    def _create_decision_logs(self, test_case: TestCase, decision_result: DecisionResult, biasedness: float, shuffled_answer_options: bool) -> dict:
+        """
+        Function to be used after the decision result has been obtained for a test case. Converts a TestCase and a DecisionResult objects together into run logs.
+
+        Args:
+            test_case (TestCase): The TestCase that was used during the run.
+            decision_result (DecisionResult): The DecisionResult object containing the final model decisions from the run.
+            biasedness (float): The biasedness value calculated by the bias metric.
+            shuffled_answer_options (bool): Whether the answer options were randomly shuffled for this run.
+
+        Returns:
+            dict: A dictionary of run logs.
+        """
 
         # Create the logs for the test case that was used
         logs = self._create_test_case_logs(test_case)
@@ -121,7 +171,7 @@ class Experiment:
         logs["Decision Model"] = decision_result.MODEL
         logs["Decision Temperature"] = decision_result.TEMPERATURE
         logs["Decision Seed"] = decision_result.SEED
-        logs["Shuffled Answer Options"] = self.shuffle_answer_options
+        logs["Shuffled Answer Options"] = shuffled_answer_options
 
         logs["Control Options"] = decision_result.CONTROL_OPTIONS
         logs["Control Option Order"] = decision_result.CONTROL_OPTION_ORDER
@@ -138,18 +188,34 @@ class Experiment:
         return logs
 
     def _start(self, name: str):
-        # Start a new wandb run
+        """
+        Starts tracking of a new run on Weights & Biases
+
+        Args:
+            name (str): Name of the new run.
+        """
+
         wandb.init(
             entity=WANDB_ENTITY,
             project=WANDB_PROJECT,
-            name=name,
-            config={}
+            name=name
         )
 
     def _log(self, metrics: dict):
+        """
+        Logs some metrics to Weights & Biases for the current run.
+
+        Args:
+            metrics (dict): A dictionary of metrics to log to the current run.
+        """
+
         wandb.log(metrics)
 
     def _finish(self):
+        """
+        Finishes the tracked run on Weights & Biases.
+        """
+
         wandb.finish()
 
 
