5bb25f7f10aba3a5a4bc1b277dc037dfb5e1036c
Roman Poletukhin <poletukhin.rs@phystech.edu>
2024-08-25 16:20:08 +0200

    EscalationOfCommitment added

---
+++ biases/EscalationOfCommitment/test.py
@@ -0,0 +1,165 @@
+from base import TestGenerator, LLM, Metric, MetricCalculationError
+from tests import TestCase, Template, TestConfig, DecisionResult
+import numpy as np
+import re
+
+
+class EscalationOfCommitmentTestGenerator(TestGenerator):
+    """
+    Test generator for the Escalation Of Commitment.
+
+    Attributes:
+        BIAS (str): The cognitive bias associated with this test generator.
+        config (TestConfig): The test configuration for this cognitive bias.
+    """
+
+    def __init__(self):
+        self.BIAS: str = "Escalation Of Commitment"
+        self.config: TestConfig = super().load_config(self.BIAS)
+
+    def generate_all(
+        self, model: LLM, scenarios: list[str], seed: int = 42
+    ) -> list[TestCase]:
+        # Create test cases for all scenarios
+        test_cases: list[TestCase] = []
+        for scenario in scenarios:
+            try:
+                test_case = self.generate(model, scenario, None, seed)
+                test_cases.append(test_case)
+            except Exception as e:
+                print(
+                    f"Generating the test case failed.\nScenario: {scenario}\nSeed: {seed}"
+                )
+                print(e)
+
+        return test_cases
+
+    def generate(
+        self, model: LLM, scenario: str, custom_values: dict = {}, seed: int = 42
+    ) -> TestCase:
+        # Load the control and treatment templates
+        control: Template = self.config.get_control_template()
+        treatment: Template = self.config.get_treatment_template()
+
+        # Populate the templates using the model and the scenario
+        control, treatment = super().populate(model, control, treatment, scenario)
+
+        # Create a test case object
+        test_case = TestCase(
+            bias=self.BIAS,
+            control=control,
+            treatment=treatment,
+            generator=model.NAME,
+            scenario=scenario,
+            control_values=None,
+            treatment_values=None,
+            variant=None,
+            remarks=None,
+        )
+
+        return test_case
+
+
+class EscalationOfCommitmentMetric(Metric):
+    """
+    Metric calculator for the Escalation of Commitment (Commitment Bias).
+
+    Metric:
+    𝔅 = (â₁ - â₂) / a
+
+    where:
+    â₁, â₂ are the chosen answers for the control and treatment versions, respectively;
+    a = â₁ - â (if â₁ - â₂ >= 0) or else a = ã - â₁, where ã is the maximum option, â - the minimum option.
+
+    """
+
+    def _compute(
+        self,
+        control_answer: np.array,
+        treatment_answer: np.array,
+        max_option: np.array,
+        min_option: np.array,
+    ) -> np.array:
+        """
+        Compute the metric for the Escalation of Commitment.
+
+        Args:
+            control_answer (np.array): The answer chosen in the control version.
+            treatment_answer (np.array): The answer chosen in the treatment version.
+            max_option (np.array): The maximum answer option.
+            min_option (np.array): The minimum answer option.
+
+        Returns:
+            np.array: The metric value for the test case.
+        """
+        delta = control_answer - treatment_answer
+        biasedness = delta / (
+            (delta >= 0) * (control_answer - min_option)
+            + (delta < 0) * (max_option - control_answer)
+            + 10e-8
+        )
+
+        return biasedness
+
+    def assemble_options(self, options_list: list[dict]) -> np.array:
+        """
+        Assemble the answer options into a numpy array.
+
+        Args:
+            options (dict): The answer options for the test case.
+
+        Returns:
+            np.array: The assembled numerical answer options array.
+        """
+        answer_options = np.array([])
+        for options in options_list:
+            numerical_options = [int(re.findall(r"-?\d+\.?\d*", s)[0]) for s in options]
+            if not answer_options.size:
+                answer_options = np.array([numerical_options])
+            else:
+                answer_options = np.vstack((answer_options, numerical_options))
+
+        return answer_options
+
+    def compute(self, test_results: list[tuple[TestCase, DecisionResult]]) -> float:
+        # make sure all pairs are not None
+        test_results = [
+            pair for pair in test_results if pair[0] is not None and pair[1] is not None
+        ]
+        try:
+            # extract answer options from the test results
+            answer_options = self.assemble_options(
+                [
+                    decision_result.CONTROL_OPTIONS
+                    for (_, decision_result) in test_results
+                ]
+            )
+            max_option = np.max(answer_options, axis=1)
+            min_option = np.min(answer_options, axis=1)
+            # extract indices of the chosen answers
+            control_answer_idx = np.array(
+                [
+                    [decision_result.CONTROL_DECISION]
+                    for (_, decision_result) in test_results
+                ]
+            )
+            treatment_answer_idx = np.array(
+                [
+                    [decision_result.TREATMENT_DECISION]
+                    for (_, decision_result) in test_results
+                ]
+            )
+            # extract the chosen answers
+            control_answer = np.take_along_axis(
+                answer_options, control_answer_idx, axis=1
+            )
+            treatment_answer = np.take_along_axis(
+                answer_options, treatment_answer_idx, axis=1
+            )
+            biasedness_scores = np.mean(
+                self._compute(control_answer, treatment_answer, max_option, min_option)
+            )
+        except Exception as e:
+            print(e)
+            raise MetricCalculationError("The metric could not be computed.")
+        return np.around(biasedness_scores, 2)
