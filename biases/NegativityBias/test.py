c61fccd26382ce1ec74f955966794033f3c8b71b
Roman Poletukhin <poletukhin.rs@phystech.edu>
2024-09-12 16:41:03 +0200

    NegativityBias revisited

--- biases/NegativityBias/test.py
+++ biases/NegativityBias/test.py
@@ -1,7 +1,6 @@
 from base import TestGenerator, LLM, Metric, MetricCalculationError
 from tests import TestCase, Template, TestConfig, DecisionResult
 import numpy as np
-import re
 
 
 class NegativityBiasTestGenerator(TestGenerator):
@@ -51,8 +50,6 @@ class NegativityBiasTestGenerator(TestGenerator):
             treatment=treatment,
             generator=model.NAME,
             scenario=scenario,
-            control_values=None,
-            treatment_values=None,
             variant=None,
             remarks=None,
         )
@@ -69,16 +66,12 @@ class NegativityBiasMetric(Metric):
 
     where:
     â₁, â₂ are the chosen answers for the control and treatment versions, respectively;
-    a = â₁ - â (if â₂ - â₁ < 0) or else a = ã - â₁, where ã is the maximum option, â - the minimum option.
+    a = â₁ - â (if â₂ - â₁ < 0) or else a = ã - â₁, where ã is the maximum option, â - the minimum option (= 0).
 
     """
 
     def _compute(
-        self,
-        control_answer: np.array,
-        treatment_answer: np.array,
-        max_option: np.array,
-        min_option: np.array,
+        self, control_answer: np.array, treatment_answer: np.array, max_option: np.array
     ) -> np.array:
         """
         Compute the metric for the Negativity bias.
@@ -87,7 +80,6 @@ class NegativityBiasMetric(Metric):
             control_answer (np.array): The answer chosen in the control version.
             treatment_answer (np.array): The answer chosen in the treatment version.
             max_option (np.array): The maximum answer option.
-            min_option (np.array): The minimum answer option.
 
         Returns:
             np.array: The metric value for the test case.
@@ -95,32 +87,11 @@ class NegativityBiasMetric(Metric):
         delta = treatment_answer - control_answer
         metric_value = delta / (
             (delta >= 0) * (max_option - control_answer)
-            + (delta < 0) * (control_answer - min_option)
+            + (delta < 0) * control_answer
             + 10e-8
         )
 
         return metric_value
-
-    # TODO: consider moving this method to the base class if it is used in multiple metrics
-    def assemble_options(self, options_list: list[dict]) -> np.array:
-        """
-        Assemble the answer options into a numpy array.
-
-        Args:
-            options (dict): The answer options for the test case.
-
-        Returns:
-            np.array: The assembled answer options array.
-        """
-        answer_options = np.array([])
-        for options in options_list:
-            numerical_options = [int(re.findall(r"-?\d+\.?\d*", s)[0]) for s in options]
-            if not answer_options.size:
-                answer_options = np.array([numerical_options])
-            else:
-                answer_options = np.vstack((answer_options, numerical_options))
-
-        return answer_options
 
     def compute(self, test_results: list[tuple[TestCase, DecisionResult]]) -> float:
         try:
@@ -130,39 +101,25 @@ class NegativityBiasMetric(Metric):
                 for pair in test_results
                 if pair[0] is not None and pair[1] is not None
             ]
-            # extract answer options from the test results
-            answer_options = self.assemble_options(
-                [
-                    decision_result.CONTROL_OPTIONS
-                    for (_, decision_result) in test_results
-                ]
-            )
-            max_option = np.max(answer_options, axis=1)
-            min_option = np.min(answer_options, axis=1)
-            # extract indices of the chosen answers
-            control_answer_idx = np.array(
+            # extract the max option
+            max_option = len(test_results[0][1].CONTROL_OPTIONS) - 1
+            # extract answers
+            control_answer = np.array(
                 [
                     [decision_result.CONTROL_DECISION]
                     for (_, decision_result) in test_results
                 ]
             )
-            treatment_answer_idx = np.array(
+            treatment_answer = np.array(
                 [
                     [decision_result.TREATMENT_DECISION]
                     for (_, decision_result) in test_results
                 ]
             )
-            # extract the chosen answers
-            control_answer = np.take_along_axis(
-                answer_options, control_answer_idx, axis=1
-            )
-            treatment_answer = np.take_along_axis(
-                answer_options, treatment_answer_idx, axis=1
-            )
             biasedness_scores = np.mean(
-                self._compute(control_answer, treatment_answer, max_option, min_option)
+                self._compute(control_answer, treatment_answer, max_option)
             )
         except Exception as e:
             print(e)
-            raise MetricCalculationError("The metric could not be computed.")
+            raise MetricCalculationError(f"Error computing the metric: {e}")
         return round(biasedness_scores, 2)
