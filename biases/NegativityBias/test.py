a1a6e229f0b01424d2640cbc1b74364a5965d1b4
Roman Poletukhin <poletukhin.rs@phystech.edu>
2024-09-29 20:26:07 +0200

    NegativityBias finalised

--- biases/NegativityBias/test.py
+++ biases/NegativityBias/test.py
@@ -1,4 +1,4 @@
-from base import TestGenerator, LLM, Metric, MetricCalculationError
+from base import TestGenerator, LLM, RatioScaleMetric, MetricCalculationError
 from tests import TestCase, Template, TestConfig, DecisionResult
 import numpy as np
 
@@ -16,32 +16,23 @@ class NegativityBiasTestGenerator(TestGenerator):
         self.BIAS: str = "Negativity Bias"
         self.config: TestConfig = super().load_config(self.BIAS)
 
-    def generate_all(
-        self, model: LLM, scenarios: list[str], seed: int = 42
-    ) -> list[TestCase]:
-        # Create test cases for all scenarios
-        test_cases: list[TestCase] = []
-        for scenario in scenarios:
-            try:
-                test_case = self.generate(model, scenario, None, seed)
-                test_cases.append(test_case)
-            except Exception as e:
-                print(
-                    f"Generating the test case failed.\nScenario: {scenario}\nSeed: {seed}"
-                )
-                print(e)
-
-        return test_cases
-
     def generate(
-        self, model: LLM, scenario: str, config_values: dict = {}, seed: int = 42
+        self,
+        model: LLM,
+        scenario: str,
+        custom_values: dict = {},
+        step: int = 0,
+        temperature: float = 0.0,
+        seed: int = 42,
     ) -> TestCase:
         # Load the control and treatment templates
         control: Template = self.config.get_control_template()
         treatment: Template = self.config.get_treatment_template()
 
-        # Populate the templates using the model and the scenario (note that we first populate treatment and then control here)
-        control, treatment = super().populate(model, control, treatment, scenario)
+        # Populate the templates using the model and the scenario
+        control, treatment = super().populate(
+            model, control, treatment, scenario, temperature, seed
+        )
 
         # Create a test case object
         test_case = TestCase(
@@ -49,6 +40,8 @@ class NegativityBiasTestGenerator(TestGenerator):
             control=control,
             treatment=treatment,
             generator=model.NAME,
+            temperature=temperature,
+            seed=seed,
             scenario=scenario,
             variant=None,
             remarks=None,
@@ -57,69 +50,19 @@ class NegativityBiasTestGenerator(TestGenerator):
         return test_case
 
 
-class NegativityBiasMetric(Metric):
+class NegativityBiasMetric(RatioScaleMetric):
     """
-    A class that describes the quantitative evaluation of the negativity bias in a model.
+    A class that describes the quantitative evaluation of the Negativity bias in a model.
 
     Metric:
-    𝔅 = (â₂ - â₁) / a
+    𝔅(â₁, â₂) = (â₂ - â₁) / max(â₁, â₂) ∈ [-1, 1]
 
     where:
     â₁, â₂ are the chosen answers for the control and treatment versions, respectively;
-    a = â₁ - â (if â₂ - â₁ < 0) or else a = ã - â₁, where ã is the maximum option, â - the minimum option (= 0).
 
+    Attributes:
+        test_results (list[tuple[TestCase, DecisionResult]]): A list of test results to be used for the metric calculation.
     """
 
-    def _compute(
-        self, control_answer: np.array, treatment_answer: np.array, max_option: np.array
-    ) -> np.array:
-        """
-        Compute the metric for the Negativity bias.
-
-        Args:
-            control_answer (np.array): The answer chosen in the control version.
-            treatment_answer (np.array): The answer chosen in the treatment version.
-            max_option (np.array): The maximum answer option.
-
-        Returns:
-            np.array: The metric value for the test case.
-        """
-        delta = treatment_answer - control_answer
-        metric_value = delta / (
-            (delta >= 0) * (max_option - control_answer)
-            + (delta < 0) * control_answer
-            + 10e-8
-        )
-
-        return metric_value
-
-    def compute(self, test_results: list[tuple[TestCase, DecisionResult]]) -> float:
-        try:
-            # make sure all pairs are not None
-            test_results = [
-                pair
-                for pair in test_results
-                if pair[0] is not None and pair[1] is not None
-            ]
-            # extract the max option
-            max_option = len(test_results[0][1].CONTROL_OPTIONS) - 1
-            # extract answers
-            control_answer = np.array(
-                [
-                    [decision_result.CONTROL_DECISION]
-                    for (_, decision_result) in test_results
-                ]
-            )
-            treatment_answer = np.array(
-                [
-                    [decision_result.TREATMENT_DECISION]
-                    for (_, decision_result) in test_results
-                ]
-            )
-            biasedness_scores = np.mean(
-                self._compute(control_answer, treatment_answer, max_option)
-            )
-        except Exception as e:
-            print(e)
-            raise MetricCalculationError(f"Error computing the metric: {e}")
-        return round(biasedness_scores, 2)
+    def __init__(self, test_results: list[tuple[TestCase, DecisionResult]]):
+        super().__init__(test_results)
