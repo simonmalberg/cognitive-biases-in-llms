16ed648e0acff5c74f3943d5bea0a2ae3ed32f4f
Roman Poletukhin <poletukhin.rs@phystech.edu>
2024-09-30 01:55:54 +0200

    HyperbolicDiscounting finalised

--- biases/HyperbolicDiscounting/test.py
+++ biases/HyperbolicDiscounting/test.py
@@ -1,4 +1,4 @@
-from base import TestGenerator, LLM, Metric, MetricCalculationError
+from base import TestGenerator, LLM, RatioScaleMetric
 from tests import TestCase, Template, TestConfig, DecisionResult
 import numpy as np
 
@@ -16,75 +16,94 @@ class HyperbolicDiscountingTestGenerator(TestGenerator):
         self.BIAS: str = "Hyperbolic Discounting"
         self.config: TestConfig = super().load_config(self.BIAS)
 
-    def generate_all(
-        self, model: LLM, scenarios: list[str], seed: int = 42
-    ) -> list[TestCase]:
-        # Load the custom values from the test config
-        custom_values = self.config.get_custom_values()
-        # Create test cases for all scenarios
-        test_cases: list[TestCase] = []
-        for scenario in scenarios:
-            try:
-                test_case = self.generate(model, scenario, custom_values, seed)
-                test_cases.append(test_case)
-            except Exception as e:
-                print(
-                    f"Generating the test case failed.\nScenario: {scenario}\nSeed: {seed}"
-                )
-                print(e)
+    def sample_custom_values(self, num_instances: int, iteration_seed: int) -> dict:
+        """
+        Sample custom values for the test case generation.
 
-        return test_cases
+        Args:
+            num_instances (int): The number of instances expected to be generated for each scenario.
+            iteration_seed (int): The seed to use for sampling the custom values.
+
+        Returns:
+            dict: A dictionary containing the sampled custom values.
+        """
+        np.random.seed(iteration_seed)
+        # load the custom values for this test
+        custom_values = self.config.get_custom_values()
+        # randomly sample each custom value 'num_instances' number of times
+        # in this case, we are sampling the earlier_reward, later_coef, months_delay and the schemes' order
+        index = np.random.choice(
+            range(len(custom_values["scheme_control"])), size=num_instances
+        )
+        sampled_values = {"index": index}
+        for key, value in custom_values.items():
+            if key == "scheme_control" or key == "scheme_treatment":
+                sampled_values[key] = value
+            else:
+                sampled_values[key] = getattr(np.random, value[0])(
+                    int(value[1]), int(value[2]), size=num_instances
+                )
+        # Making sure the later reward is always less than 100
+        sampled_values["later_reward"] = np.minimum(
+            sampled_values["earlier_reward"] * np.array(sampled_values["later_coef"]),
+            100,
+        ).astype(int)
+
+        return sampled_values
 
     def generate(
-        self, model: LLM, scenario: str, custom_values: dict = {}, seed: int = 42
+        self,
+        model: LLM,
+        scenario: str,
+        custom_values: dict = {},
+        step: int = 0,
+        temperature: float = 0.0,
+        seed: int = 42,
     ) -> TestCase:
         # Load the control and treatment templates
         control: Template = self.config.get_control_template()
         treatment: Template = self.config.get_treatment_template()
 
-        # Populate the templates with custom values
-        np.random.seed(seed)
-        earlier_values, later_values, delay_values, scheme_control, scheme_treatment = (
+        # Load the custom values sampled in the sample_custom_values method
+        (
+            earlier_reward,
+            later_reward,
+            months_delay,
+            scheme_control,
+            scheme_treatment,
+            index,
+        ) = (
             custom_values["earlier_reward"],
-            custom_values["later_coef"],
+            custom_values["later_reward"],
             custom_values["months_delay"],
-            custom_values['scheme_control'],
-            custom_values['scheme_treatment']
+            custom_values["scheme_control"],
+            custom_values["scheme_treatment"],
+            custom_values["index"],
         )
-        # Sampling the order of the schemes
-        index = np.random.choice(range(len(scheme_control)))
+        # Insert the custom values that correspond to the current step into the template
         for template in [control, treatment]:
-            template.insert("control_scheme", scheme_control[index], origin="user")
-            template.insert("other_control_scheme", scheme_control[1 - index], origin="user")
-            template.insert("treatment_scheme", scheme_treatment[index], origin="user")
-            template.insert("other_treatment_scheme", scheme_treatment[1 - index], origin="user")
-            
-        # Loading the required distributions (should be np.random methods)
-        earlier_distribution, later_distribution, delay_distribution = (
-            getattr(np.random, earlier_values[0]),
-            getattr(np.random, later_values[0]),
-            getattr(np.random, delay_values[0]),
-        )
-        # Sampling the respective numerical values
-        earlier_reward = earlier_distribution(
-            int(earlier_values[1]), int(earlier_values[2])
-        )
-        later_reward = min(
-            100,
-            int(
-                earlier_reward
-                * later_distribution(int(later_values[1]), int(later_values[2]))
-            ),
-        )
-        months_delay = delay_distribution(int(delay_values[1]), int(delay_values[2]))
-        # Inserting the sampled value into the template
-        for template in [control, treatment]:
-            template.insert("earlier_reward", str(earlier_reward), origin="user")
-            template.insert("later_reward", str(later_reward), origin="user")
-            template.insert("months_delay", str(months_delay), origin="user")
+            template.insert(
+                "control_scheme", scheme_control[index[step]], origin="user"
+            )
+            template.insert(
+                "other_control_scheme", scheme_control[1 - index[step]], origin="user"
+            )
+            template.insert(
+                "treatment_scheme", scheme_treatment[index[step]], origin="user"
+            )
+            template.insert(
+                "other_treatment_scheme",
+                scheme_treatment[1 - index[step]],
+                origin="user",
+            )
+            template.insert("earlier_reward", str(earlier_reward[step]), origin="user")
+            template.insert("later_reward", str(later_reward[step]), origin="user")
+            template.insert("months_delay", str(months_delay[step]), origin="user")
 
         # Populate the templates using the model and the scenario
-        control, treatment = super().populate(model, control, treatment, scenario)
+        control, treatment = super().populate(
+            model, control, treatment, scenario, temperature, seed
+        )
 
         # Create a test case object
         test_case = TestCase(
@@ -92,6 +111,8 @@ class HyperbolicDiscountingTestGenerator(TestGenerator):
             control=control,
             treatment=treatment,
             generator=model.NAME,
+            temperature=temperature,
+            seed=seed,
             scenario=scenario,
             variant=None,
             remarks=None,
@@ -100,57 +121,30 @@ class HyperbolicDiscountingTestGenerator(TestGenerator):
         return test_case
 
 
-class HyperbolicDiscountingMetric(Metric):
+class HyperbolicDiscountingMetric(RatioScaleMetric):
     """
     A class that describes the quantitative evaluation of the Hyperbolic Discounting in a model.
 
     Metric:
-    𝔅 = â₂ - â₁ ∈ {-1, 0, 1}
+    𝔅(â₁, â₂) = k ⋅ (â₂ - â₁) / max(â₁, â₂) ∈ [-1, 1]
     where:
     â₂, â₁ are the chosen answers for the treatment and control versions, respectively.
+    k is the parameter that reflects the order of schemes in the test case ()
+
+    Attributes:
+        test_results (list[tuple[TestCase, DecisionResult]]): The list of test results to be used for the metric calculation.
     """
 
-    def _compute(
-        self, control_answer: np.array, treatment_answer: np.array
-    ) -> np.array:
-        """
-        Compute the metric for the Hyperbolic Discounting.
-
-        Args:
-            control_answer (np.array): The answer chosen in the control version.
-            treatment_answer (np.array): The answer chosen in the treatment version.
-
-        Returns:
-            np.array: The metric value for the test case.
-        """
-        metric_value = treatment_answer - control_answer
-
-        return metric_value
-
-    def compute(self, test_results: list[tuple[TestCase, DecisionResult]]) -> float:
-        try:
-            # make sure all pairs are not None
-            test_results = [
-                pair
-                for pair in test_results
-                if pair[0] is not None and pair[1] is not None
+    def __init__(self, test_results: list[tuple[TestCase, DecisionResult]]):
+        super().__init__(test_results)
+        # set the coefficient in the metric: it depends on the 'index' custom value that we sampled
+        # (and reflects which scheme is presented first, i.e., which scheme is A)
+        self.k = [
+            [
+                insertion.text
+                for insertion in test_case.CONTROL.get_insertions()
+                if insertion.pattern == "control_scheme"
             ]
-            # extract chosen answers
-            control_answer = np.array(
-                [
-                    decision_result.CONTROL_DECISION
-                    for (_, decision_result) in test_results
-                ]
-            )
-            treatment_answer = np.array(
-                [
-                    decision_result.TREATMENT_DECISION
-                    for (_, decision_result) in test_results
-                ]
-            )
-            # compute the biasedness scores
-            biasedness_scores = np.mean(self._compute(control_answer, treatment_answer))
-        except Exception as e:
-            print(e)
-            raise MetricCalculationError(f"Error computing the metric: {e}")
-        return biasedness_scores
+            for (test_case, _) in self.test_results
+        ]
+        self.k = np.array([[-1] if "immediately" in k[0] else [1] for k in self.k])
