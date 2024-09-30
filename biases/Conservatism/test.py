cf3d591193ecaa78f70bec484d606bc12ae36ed9
simaex.unknown <simaex.unknown@googlemail.com>
2024-09-30 19:06:11 +0000

    Revisited the test for Conservatism

--- biases/Conservatism/test.py
+++ biases/Conservatism/test.py
@@ -1,4 +1,4 @@
-from base import TestGenerator, LLM, Metric
+from base import TestGenerator, LLM, RatioScaleMetric
 from tests import TestCase, Template, TestConfig, DecisionResult
 import numpy as np
 
@@ -16,20 +16,7 @@ class ConservatismTestGenerator(TestGenerator):
         self.BIAS: str = "Conservatism"
         self.config: TestConfig = super().load_config(self.BIAS)
 
-    def generate_all(self, model: LLM, scenarios: list[str], seed: int = 42) -> list[TestCase]:
-        # Create test cases for all scenarios
-        test_cases: list[TestCase] = []
-        for scenario in scenarios:
-            try:
-                test_case = self.generate(model, scenario, {}, seed)
-                test_cases.append(test_case)
-            except Exception as e:
-                print(f"Generating the test case failed.\nScenario: {scenario}\nSeed: {seed}")
-                print(e)
-
-        return test_cases
-
-    def generate(self, model: LLM, scenario: str, config_values: dict = {}, seed: int = 42) -> TestCase:
+    def generate(self, model: LLM, scenario: str, custom_values: dict = {}, temperature: float = 0.0, seed: int = 42) -> TestCase:
         # Load the control and treatment templates
         control: Template = self.config.get_control_template()
         treatment: Template = self.config.get_treatment_template()
@@ -43,9 +30,9 @@ class ConservatismTestGenerator(TestGenerator):
             control=control,
             treatment=treatment,
             generator=model.NAME,
+            temperature=temperature,
+            seed=seed,
             scenario=scenario,
-            control_values=None,
-            treatment_values=None,
             variant=None,
             remarks=None
         )
@@ -53,39 +40,13 @@ class ConservatismTestGenerator(TestGenerator):
         return test_case
 
 
-class ConservatismMetric(Metric):
+class ConservatismMetric(RatioScaleMetric):
+    """
+    A metric that measures the presence and strength of Conservatism based on a set of test results.
 
-    def __init__(self):
-        pass
+    Attributes:
+        test_results (list[tuple[TestCase, DecisionResult]]): The list of test results to be used for the metric calculation.
+    """
 
-    def _compute(self, test_result: tuple[TestCase, DecisionResult]) -> float:
-        # Extract the decision result from the tuple
-        decision_result: DecisionResult = test_result[1]
-
-        # Extract the control and treatment decisions
-        control_preference = decision_result.CONTROL_DECISION
-        treatment_preference = decision_result.TREATMENT_DECISION
-
-        # Get the number of available options to calculate the center and diameter of the scale for control
-        SCALE_OPTIONS_CONTROL = len(decision_result.CONTROL_OPTIONS)
-        SCALE_DIAMETER_CONTROL = SCALE_OPTIONS_CONTROL // 2
-        SCALE_CENTER_CONTROL = SCALE_DIAMETER_CONTROL + 1
-
-        # Do the same for treatment
-        SCALE_OPTIONS_TREATMENT = len(decision_result.TREATMENT_OPTIONS)
-        SCALE_DIAMETER_TREATMENT = SCALE_OPTIONS_TREATMENT // 2
-        SCALE_CENTER_TREATMENT = SCALE_DIAMETER_TREATMENT + 1
-
-        # Calculate the normalized biasedness for both, the control and the treatment decision
-        bias_control = -1 * (control_preference - SCALE_CENTER_CONTROL) / SCALE_DIAMETER_CONTROL
-        bias_treatment = -1 * (treatment_preference - SCALE_CENTER_TREATMENT) / SCALE_DIAMETER_TREATMENT
-
-        # Calculate the average biasenedness of the control and the treatment decision
-        biasedness = (bias_control + bias_treatment) / 2
-
-        return biasedness
-
-    def compute(self, test_results: list[tuple[TestCase, DecisionResult]]) -> float:
-        # Calculate the average biasedness score across all tests
-        biasedness_scores = [self._compute(test_result) for test_result in test_results]
-        return np.mean(biasedness_scores)
+    def __init__(self, test_results: list[tuple[TestCase, DecisionResult]]):
+        super().__init__(test_results, flip_treatment=True)
