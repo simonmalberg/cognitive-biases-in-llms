b0222e25c9c6f4ec9b3e2e1de7b37c1ba1d16439
Roman Poletukhin <poletukhin.rs@phystech.edu>
2024-10-01 00:59:15 +0200

    OptimismBias finalised

--- biases/OptimismBias/test.py
+++ biases/OptimismBias/test.py
@@ -1,7 +1,5 @@
-from base import TestGenerator, LLM, Metric, MetricCalculationError
-from tests import TestCase, Template, TestConfig, DecisionResult
-import random
-import re
+from base import TestGenerator, LLM, RatioScaleMetric
+from tests import TestCase, Template, DecisionResult
 import numpy as np
 
 
@@ -17,48 +15,64 @@ class OptimismBiasTestGenerator(TestGenerator):
     def __init__(self):
         self.BIAS = "Optimism Bias"
         self.config = super().load_config(self.BIAS)
+        
+    def sample_custom_values(self, num_instances: int, iteration_seed: int) -> dict:
+        """
+        Sample custom values for the test case generation.
 
-    def generate_all(
-        self, model: LLM, scenarios: list[str], seed: int = 42
-    ) -> list[TestCase]:
-        # Load the custom values from the test config
+        Args:
+            num_instances (int): The number of instances expected to be generated for each scenario.
+            iteration_seed (int): The seed to use for sampling the custom values.
+
+        Returns:
+            dict: A dictionary containing the sampled custom values.
+        """
+        sampled_values = {}
+        np.random.seed(iteration_seed)
+        # load the custom values for this test
         custom_values = self.config.get_custom_values()
-        # Create test cases for all provided scenarios
-        test_cases: list[TestCase] = []
-        for scenario in scenarios:
-            try:
-                test_case = self.generate(model, scenario, custom_values, seed)
-                test_cases.append(test_case)
-            except Exception as e:
-                print(
-                    f"Generating the test case failed.\nScenario: {scenario}\nSeed: {seed}"
-                )
-                print(e)
-
-        return test_cases
+        # randomly sample each custom value 'num_instances' number of times
+        # in this case, we are sampling the event_kind
+        index = np.random.choice(
+                range(len(custom_values["event_kind"])), size=num_instances
+            )
+        for key, value in custom_values.items():
+            if key == "event_kind":
+                sampled_values[key] = [
+                    value[index[n]] for n in range(num_instances)
+                ]
+                
+        return sampled_values
 
     def generate(
-        self, model: LLM, scenario: str, custom_values: dict = {}, seed: int = 42
+        self,
+        model: LLM,
+        scenario: str,
+        custom_values: dict = {},
+        temperature: float = 0.0,
+        seed: int = 42,
     ) -> TestCase:
+        # Load the control and treatment templates
         control: Template = self.config.get_control_template()
         treatment: Template = self.config.get_treatment_template()
 
-        # Loading the possible event kind
-        event_kinds = custom_values["event_kind"]
-        random.seed(seed)
-        # Sampling one of ['positive', 'negative']
-        chosen_kind = random.choice(event_kinds)
-        # Inserting the outcomes and amounts into the template
+        # Insert the custom values into the template
         for template in [control, treatment]:
-            template.insert("event_kind", chosen_kind, origin="user")
+            template.insert("event_kind", custom_values['event_kind'], origin="user")
 
-        control, treatment = super().populate(model, control, treatment, scenario)
-
+        # Populate the templates using the model and the scenario
+        control, treatment = super().populate(
+            model, control, treatment, scenario, temperature, seed
+        )
+        
+        # Create a test case object
         test_case = TestCase(
             bias=self.BIAS,
             control=control,
             treatment=treatment,
             generator=model.NAME,
+            temperature=temperature,
+            seed=seed,
             scenario=scenario,
             variant=None,
             remarks=None,
@@ -67,88 +81,31 @@ class OptimismBiasTestGenerator(TestGenerator):
         return test_case
 
 
-class OptimismBiasMetric(Metric):
+class OptimismBiasMetric(RatioScaleMetric):
     """
     A class that describes the quantitative evaluation of the optimism bias in a model.
 
     Individual metric:
-    𝔅 = k * (â₂ − â₁) / ‖ â₁ − ã₁ ‖₁ ∈ [0, 1];
+    𝔅(â₁, â₂) = k ⋅ (â₁ - â₂) / max(â₁, â₂) ∈ [-1, 1]
 
     where:
     â₁, â₂ are the chosen answers for the control and treatment versions, respectively;
-    ã₁ is the farthest answer option from â₁ (either 0 or max option);
-    k is the kind of event (1: positive or -1: negative).
+    k is the kind of event (-1: positive or 1: negative).
+    
+    Attributes:
+        test_results (list[tuple[TestCase, DecisionResult]]): The list of test results to be used for the metric calculation.
     """
 
-    def _compute(
-        self,
-        control_answer: np.array,
-        treatment_answer: np.array,
-        event_kind: np.array,
-        max_option: np.array,
-    ) -> np.array:
-        """
-        Compute the metric for the optimism bias.
-
-        Args:
-            control_answer (np.array): The answer from the control version.
-            treatment_answer (np.array): The answer from the treatment version.
-            event_kind (np.array): The kind of event (1: positive or -1: negative).
-            max_option (np.array): The maximum answer option.
-
-        Returns:
-            np.array: The computed metric for the optimism bias.
-        """
-        delta = treatment_answer - control_answer
-        metric_value = (
-            event_kind
-            * delta
-            / (
-                (delta >= 0) * (max_option - control_answer)
-                + (delta < 0) * (control_answer)
-                + 10e-8
-            )
-        )
-
-        return metric_value
-
-    def compute(self, test_results: list[tuple[TestCase, DecisionResult]]) -> float:
-        try:
-            # make sure all pairs are not None
-            test_results = [
-                pair
-                for pair in test_results
-                if pair[0] is not None and pair[1] is not None
+    def __init__(self, test_results: list[tuple[TestCase, DecisionResult]]):
+        super().__init__(test_results)
+        # set the coefficient in the metric: it depends on the 'index' custom value that we sampled
+        # (and reflects which event kind is used in the test case)
+        self.k = [
+            [
+                insertion.text
+                for insertion in test_case.TREATMENT.get_insertions()
+                if insertion.pattern == "event_kind"
             ]
-            max_option = len(test_results[0][1].CONTROL_OPTIONS) - 1
-            # extract chosen answers
-            control_answer = np.array(
-                [
-                    [decision_result.CONTROL_DECISION]
-                    for (_, decision_result) in test_results
-                ]
-            )
-            treatment_answer = np.array(
-                [
-                    [decision_result.TREATMENT_DECISION]
-                    for (_, decision_result) in test_results
-                ]
-            )
-            event_kind = np.array(
-                [
-                    [
-                        1 if insertion.text == "positive" else -1
-                        for insertion in test_case.TREATMENT.get_insertions()
-                        if insertion.pattern == "event_kind"
-                    ]
-                    for (test_case, _) in test_results
-                ]
-            )
-            # compute the metric and average the results
-            biasedness_scores = np.mean(
-                self._compute(control_answer, treatment_answer, event_kind, max_option)
-            )
-        except Exception as e:
-            print(e)
-            raise MetricCalculationError(f"Error computing the metric: {e}")
-        return round(biasedness_scores, 2)
+            for (test_case, _) in self.test_results
+        ]
+        self.k = np.array([[-1] if "positive" in k[0] else [1] for k in self.k])
