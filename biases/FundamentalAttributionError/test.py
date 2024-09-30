601ee0a173076bb5f308e2539b7eb3c505a0595b
Roman Poletukhin <poletukhin.rs@phystech.edu>
2024-09-30 21:18:18 +0200

    FAE analysed

--- biases/FundamentalAttributionError/test.py
+++ biases/FundamentalAttributionError/test.py
@@ -1,4 +1,4 @@
-from base import TestGenerator, LLM, Metric, MetricCalculationError
+from base import TestGenerator, LLM, RatioScaleMetric
 from tests import TestCase, Template, TestConfig, DecisionResult
 import numpy as np
 
@@ -15,49 +15,70 @@ class FundamentalAttributionErrorTestGenerator(TestGenerator):
     def __init__(self):
         self.BIAS: str = "Fundamental Attribution Error"
         self.config: TestConfig = super().load_config(self.BIAS)
+        
+    def sample_custom_values(self, num_instances: int, iteration_seed: int) -> dict:
+        """
+        Sample custom values for the test case generation.
 
-    def generate_all(
-        self, model: LLM, scenarios: list[str], seed: int = 42
-    ) -> list[TestCase]:
-        # Load the custom values from the test config
+        Args:
+            num_instances (int): The number of instances expected to be generated for each scenario.
+            iteration_seed (int): The seed to use for sampling the custom values.
+
+        Returns:
+            dict: A dictionary containing the sampled custom values.
+        """
+        sampled_values = {}
+        np.random.seed(iteration_seed)
+        # load the custom values for this test
         custom_values = self.config.get_custom_values()
-        # Create test cases for all scenarios
-        test_cases: list[TestCase] = []
-        for scenario in scenarios:
-            try:
-                test_case = self.generate(model, scenario, custom_values, seed)
-                test_cases.append(test_case)
-            except Exception as e:
-                print(
-                    f"Generating the test case failed.\nScenario: {scenario}\nSeed: {seed}"
-                )
-                print(e)
+        # randomly sample each custom value 'num_instances' number of times
+        # in this case, we are sampling the reasons for the control and treatment versions
+        index = int(
+            np.random.choice(
+                range(len(custom_values["reason_control"])), size=num_instances
+            )
+        )
+        for key, value in custom_values.items():
+            if key == "reason_control":
+                sampled_values["control_reason"] = [
+                    value[index] for _ in range(num_instances)
+                ]
+                sampled_values["other_control_reason"] = [
+                    value[1 - index] for _ in range(num_instances)
+                ]
+            elif key == "reason_treatment":
+                sampled_values["treatment_reason"] = [
+                    value[index] for _ in range(num_instances)
+                ]
+                sampled_values["other_treatment_reason"] = [
+                    value[1 - index] for _ in range(num_instances)
+                ]
 
-        return test_cases
+        return sampled_values
 
     def generate(
-        self, model: LLM, scenario: str, custom_values: dict = {}, seed: int = 42
+        self,
+        model: LLM,
+        scenario: str,
+        custom_values: dict = {},
+        temperature: float = 0.0,
+        seed: int = 42,
     ) -> TestCase:
         # Load the control and treatment templates
         control: Template = self.config.get_control_template()
         treatment: Template = self.config.get_treatment_template()
         
         # Populate the templates with custom values
-        np.random.seed(seed)
-        reason_control, reason_treatment = (
-            custom_values['reason_control'],
-            custom_values['reason_treatment']
-        )
-        # Sampling the order of the reasons
-        index = np.random.choice(range(len(reason_control)))
-        for template in [control, treatment]:
-            template.insert("control_reason", reason_control[index], origin="user")
-            template.insert("other_control_reason", reason_control[1 - index], origin="user")
-            template.insert("treatment_reason", reason_treatment[index], origin="user")
-            template.insert("other_treatment_reason", reason_treatment[1 - index], origin="user")
+        control.insert("control_reason", custom_values["control_reason"])
+        control.insert("other_control_reason", custom_values["other_control_reason"])
+        treatment.insert("treatment_reason", custom_values["treatment_reason"])
+        treatment.insert("other_treatment_reason", custom_values["other_treatment_reason"])
+            
 
         # Populate the templates using the model and the scenario
-        control, treatment = super().populate(model, control, treatment, scenario)
+        control, treatment = super().populate(
+            model, control, treatment, scenario, temperature, seed
+        )
 
         # Create a test case object
         test_case = TestCase(
@@ -65,9 +86,9 @@ class FundamentalAttributionErrorTestGenerator(TestGenerator):
             control=control,
             treatment=treatment,
             generator=model.NAME,
+            temperature=temperature,
+            seed=seed,
             scenario=scenario,
-            control_values=None,
-            treatment_values=None,
             variant=None,
             remarks=None,
         )
@@ -75,53 +96,30 @@ class FundamentalAttributionErrorTestGenerator(TestGenerator):
         return test_case
 
 
-class FundamentalAttributionErrorMetric(Metric):
+class FundamentalAttributionErrorMetric(RatioScaleMetric):
     """
     A class that describes the quantitative evaluation of the FAE in a model.
 
     Metric:
-    𝔅 = â₁ mod 2 - â₂ mod 2 ∈ [-1, 1]
+    𝔅(â₁, â₂) = k ⋅ (â₁ - â₂) / max(â₁, â₂) ∈ [-1, 1]
     where:
-    â₁, â₂ are the chosen answers for the control and treatment versions, respectively (0 for dispositional, 1 for situational);
+    â₂, â₁ are the chosen answers for the treatment and control versions, respectively.
+    k is the parameter that reflects the order of reasons in the test case (k = 1 if a dispositional reason is presented first, k = -1 otherwise).
+
+    Attributes:
+        test_results (list[tuple[TestCase, DecisionResult]]): The list of test results to be used for the metric calculation.
     """
 
-    def _compute(
-        self, control_answer: np.array, treatment_answer: np.array
-    ) -> np.array:
-        """
-        Compute the metric for the Confirmation bias.
-
-        Args:
-            control_answer (np.array): The answer chosen in the control version.
-            treatment_answer (np.array): The answer chosen in the treatment version.
-
-        Returns:
-            np.array: The metric value for the test case.
-        """
-        metric_value = control_answer % 2 - treatment_answer % 2
-
-        return metric_value
-
-    def compute(self, test_results: list[tuple[TestCase, DecisionResult]]) -> float:
-        try:
-            # make sure all pairs are not None
-            test_results = [
-                pair
-                for pair in test_results
-                if pair[0] is not None and pair[1] is not None
+    def __init__(self, test_results: list[tuple[TestCase, DecisionResult]]):
+        super().__init__(test_results)
+        # set the coefficient in the metric: it depends on the 'index' custom value that we sampled
+        # (and reflects which reason was presented first)
+        self.k = [
+            [
+                insertion.text
+                for insertion in test_case.CONTROL.get_insertions()
+                if insertion.pattern == "control_reason"
             ]
-            # extract strings of the chosen answers
-            control_answer = np.array([
-                [decision_result.CONTROL_DECISION]
-                for (_, decision_result) in test_results
-            ])
-            treatment_answer = np.array([
-                [decision_result.TREATMENT_DECISION]
-                for (_, decision_result) in test_results
-            ])
-            # compute the biasedness scores
-            biasedness_scores = np.mean(self._compute(control_answer, treatment_answer))
-        except Exception as e:
-            print(e)
-            raise MetricCalculationError(f"Error computing the metric: {e}")
-        return biasedness_scores
+            for (test_case, _) in self.test_results
+        ]
+        self.k = np.array([[1] if "dispositional" in k[0] else [-1] for k in self.k])
