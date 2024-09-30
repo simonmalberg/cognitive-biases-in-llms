dcc3cd4cf97f34f985eaadbf986d67c945cbdd17
Roman Poletukhin <poletukhin.rs@phystech.edu>
2024-09-30 15:49:37 +0200

    FramingEffect finalised

--- biases/FramingEffect/test.py
+++ biases/FramingEffect/test.py
@@ -1,7 +1,6 @@
-from base import TestGenerator, LLM, Metric, MetricCalculationError
+from base import TestGenerator, LLM, RatioScaleMetric, MetricCalculationError
 from tests import TestCase, Template, TestConfig, DecisionResult
 import numpy as np
-import random
 
 
 class FramingEffectTestGenerator(TestGenerator):
@@ -29,13 +28,13 @@ class FramingEffectTestGenerator(TestGenerator):
         Returns:
             dict: A dictionary containing the sampled custom values.
         """
-        random.seed(iteration_seed)
+        np.random.seed(iteration_seed)
         # load the custom values for this test
         custom_values = self.config.get_custom_values()
         # randomly sample each custom value 'num_instances' number of times
         # in this case, we are sampling the first_percentage value from randint from the provided range
         sampled_values = {
-            key: [random.randint(float(value[0]), float(value[1])) for _ in range(num_instances)]
+            key: getattr(np.random, value[0])(int(value[1]), int(value[2]), size=num_instances)
             for key, value in custom_values.items() if key == "first_percentage"
         }
 
@@ -49,7 +48,7 @@ class FramingEffectTestGenerator(TestGenerator):
         control: Template = self.config.get_control_template()
         treatment: Template = self.config.get_treatment_template()
 
-        # Populate the templates with the custom values sampled in the generate_all method
+        # Populate the templates with the custom values sampled in the sample_custom_values method
         # We retrive the value that was generated for the current step
         first_percentage = custom_values["first_percentage"][step]
         control.insert("first_percentage", str(first_percentage), origin='user')
@@ -74,77 +73,19 @@ class FramingEffectTestGenerator(TestGenerator):
         return test_case
 
 
-class FramingEffectMetric(Metric):
+class FramingEffectMetric(RatioScaleMetric):
     """
-    A class that describes the quantitative evaluation of the framing effect in a model.
+    A class that describes the quantitative evaluation of the Framing effect in a model.
 
     Metric:
-    𝔅 = (â₂ - â₁) / a
+    𝔅(â₁, â₂) = (â₂ - â₁) / max(â₁, â₂) ∈ [-1, 1]
 
     where:
-    â₁, â₂ are the chosen answers for the control and treatment versions, respectively (where Bad is 4, ..., Great is 0);
-    a = ã - â₁ (if â₂ - â₁ > 0) or else a = â₁ - â, where ã is the maximum option (0-indexed), â - the minimum option (0).
+    â₁, â₂ are the chosen answers for the control and treatment versions, respectively;
 
+    Attributes:
+        test_results (list[tuple[TestCase, DecisionResult]]): A list of test results to be used for the metric calculation.
     """
 
-    def _compute(
-        self,
-        control_answer: np.array,
-        treatment_answer: np.array,
-        max_option: np.array,
-        min_option: np.array,
-    ) -> np.array:
-        """
-        Compute the metric for the Framing Effect.
-
-        Args:
-            control_answer (np.array): The answer chosen in the control version.
-            treatment_answer (np.array): The answer chosen in the treatment version.
-            max_option (np.array): The maximum answer option.
-            min_option (np.array): The minimum answer option.
-
-        Returns:
-            np.array: The metric value for the test case.
-        """
-        delta = treatment_answer - control_answer
-        metric_value = delta / (
-            (delta >= 0) * (max_option - control_answer)
-            + (delta < 0) * (control_answer - min_option)
-            + 10e-8
-        )
-
-        return metric_value
-
-    def compute(self, test_results: list[tuple[TestCase, DecisionResult]]) -> float:
-        try:
-            # make sure all pairs are not None
-            test_results = [
-                pair
-                for pair in test_results
-                if pair[0] is not None and pair[1] is not None
-            ]
-            # extract the answer options' length
-            len_answer_options = len(test_results[0][1].CONTROL_OPTIONS)
-            min_option, max_option = 0, len_answer_options - 1
-            # extract original unshuffled indices of the chosen answers
-            control_answer = np.array(
-                [
-                    [decision_result.CONTROL_DECISION]
-                    for (_, decision_result) in test_results
-                ]
-            )
-            treatment_answer = np.array(
-                [
-                    [decision_result.TREATMENT_DECISION]
-                    for (_, decision_result) in test_results
-                ]
-            )
-            biasedness_scores = np.mean(
-                self._compute(
-                    control_answer, treatment_answer, max_option, min_option
-                )
-            )
-        except Exception as e:
-            print(e)
-            raise MetricCalculationError(f"Error computing the metric: {e}")
-        return round(biasedness_scores, 2)
+    def __init__(self, test_results: list[tuple[TestCase, DecisionResult]]):
+        super().__init__(test_results)
