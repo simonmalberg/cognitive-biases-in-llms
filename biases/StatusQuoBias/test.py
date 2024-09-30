337261d056e7c9feb3c51eac92b3c8536e19555c
simaex.unknown <simaex.unknown@googlemail.com>
2024-09-30 20:22:18 +0000

    Revisited the test for Status-Quo Bias

--- biases/StatusQuoBias/test.py
+++ biases/StatusQuoBias/test.py
@@ -1,4 +1,4 @@
-from base import TestGenerator, LLM, Metric, PopulationError, MetricCalculationError
+from base import TestGenerator, LLM, RatioScaleMetric, PopulationError
 from tests import TestCase, Template, TestConfig, DecisionResult
 import numpy as np
 
@@ -16,34 +16,13 @@ class StatusQuoBiasTestGenerator(TestGenerator):
         self.BIAS: str = "Status Quo Bias"
         self.config: TestConfig = super().load_config(self.BIAS)
 
-    def generate_all(self, model: LLM, scenarios: list[str], seed: int = 42) -> list[TestCase]:
-        # Load the custom values from the test configuration
-        custom_values = self.config.get_custom_values()
-
-        # Create test cases for all scenarios
-        test_cases: list[TestCase] = []
-        for scenario in scenarios:
-            try:
-                test_case = self.generate(model, scenario, custom_values, seed)
-                test_cases.append(test_case)
-            except Exception as e:
-                print(f"Generating the test case failed.\nScenario: {scenario}\nSeed: {seed}")
-                print(e)
-
-        return test_cases
-
-    def generate(self, model: LLM, scenario: str, config_values: dict = {}, seed: int = 42) -> TestCase:
+    def generate(self, model: LLM, scenario: str, custom_values: dict = {}, temperature: float = 0.0, seed: int = 42) -> TestCase:
         # Load the control and treatment templates
         control: Template = self.config.get_control_template()
         treatment: Template = self.config.get_treatment_template()
 
         # Generate two decision alternatives the manager could choose from
         alternatives = self._create_alternatives(model, scenario, seed)
-
-        # Insert for how many years the status quo has been in place
-        number_of_years = config_values["number_of_years"]
-        control.insert("number_of_years", str(number_of_years))
-        treatment.insert("number_of_years", str(number_of_years))
 
         # Insert the alternatives into the templates
         control.insert("status A", alternatives[0])
@@ -60,9 +39,9 @@ class StatusQuoBiasTestGenerator(TestGenerator):
             control=control,
             treatment=treatment,
             generator=model.NAME,
+            temperature=temperature,
+            seed=seed,
             scenario=scenario,
-            control_values=None,
-            treatment_values=None,
             variant=None,
             remarks=None
         )
@@ -100,40 +79,13 @@ class StatusQuoBiasTestGenerator(TestGenerator):
         return alternatives[0], alternatives[1]
 
 
-class StatusQuoBiasMetric(Metric):
+class StatusQuoBiasMetric(RatioScaleMetric):
+    """
+    A metric that measures the presence and strength of Status-Quo Bias based on a set of test results.
 
-    def __init__(self):
-        pass
+    Attributes:
+        test_results (list[tuple[TestCase, DecisionResult]]): The list of test results to be used for the metric calculation.
+    """
 
-    def _compute(self, test_result: tuple[TestCase, DecisionResult]) -> float:
-        # Extract the decision result from the tuple
-        decision_result: DecisionResult = test_result[1]
-
-        # Validate that only valid answer options were chosen
-        if decision_result.CONTROL_DECISION not in [1, 2]:
-            raise MetricCalculationError(f"Invalid answer option chosen: {decision_result.CONTROL_DECISION}. Only 1 and 2 are valid answer options.")
-        if decision_result.TREATMENT_DECISION not in [1, 2]:
-            raise MetricCalculationError(f"Invalid answer option chosen: {decision_result.TREATMENT_DECISION}. Only 1 and 2 are valid answer options.")
-
-        # Extract the control and treatment answers from the decision result
-        control_switch = decision_result.CONTROL_DECISION == 2       # True if control answer was to switch to the alternative
-        treatment_switch = decision_result.TREATMENT_DECISION == 1   # True if treatment answer was to switch to the alternative
-
-        # Calculate the biasedness
-        if control_switch:
-            if treatment_switch:
-                biasedness = -1.0
-            else:
-                biasedness = 0.0
-        else:
-            if treatment_switch:
-                biasedness = 0.0
-            else:
-                biasedness = 1.0
-
-        return biasedness
-
-    def compute(self, test_results: list[tuple[TestCase, DecisionResult]]) -> float:
-        # Calculate the average biasedness score across all tests
-        biasedness_scores = [self._compute(test_result) for test_result in test_results]
-        return np.mean(biasedness_scores)
+    def __init__(self, test_results: list[tuple[TestCase, DecisionResult]]):
+        super().__init__(test_results, flip_treatment=True)
