48621fdfc8a900ce7aef755578552e4ca9b464e7
simaex.unknown <simaex.unknown@googlemail.com>
2024-09-30 21:08:09 +0000

    Revisited the test for Reactance

--- biases/Reactance/test.py
+++ biases/Reactance/test.py
@@ -1,4 +1,4 @@
-from base import TestGenerator, LLM, Metric
+from base import TestGenerator, LLM, RatioScaleMetric
 from tests import TestCase, Template, TestConfig, DecisionResult
 import numpy as np
 
@@ -16,20 +16,7 @@ class ReactanceTestGenerator(TestGenerator):
         self.BIAS: str = "Reactance"
         self.config: TestConfig = super().load_config(self.BIAS)
 
-    def generate_all(self, model: LLM, scenarios: list[str], seed: int = 42) -> list[TestCase]:
-        # Create test cases for all scenarios
-        test_cases: list[TestCase] = []
-        for scenario in scenarios:
-            try:
-                test_case = self.generate(model, scenario, {}, seed)
-                test_cases.append(test_case)
-            except Exception as e:
-                print(f"Generating the test case failed.\nScenario: {scenario}\nSeed: {seed}")
-                print(e)
-
-        return test_cases
-
-    def generate(self, model: LLM, scenario: str, config_values: dict = {}, seed: int = 42) -> TestCase:
+    def generate(self, model: LLM, scenario: str, custom_values: dict = {}, temperature: float = 0.0, seed: int = 42) -> TestCase:
         # Load the control and treatment templates
         variant = "reduce_behavior"
         control: Template = self.config.get_control_template(variant)
@@ -44,9 +31,9 @@ class ReactanceTestGenerator(TestGenerator):
             control=control,
             treatment=treatment,
             generator=model.NAME,
+            temperature=temperature,
+            seed=seed,
             scenario=scenario,
-            control_values=None,
-            treatment_values=None,
             variant=None,
             remarks=None
         )
@@ -54,40 +41,13 @@ class ReactanceTestGenerator(TestGenerator):
         return test_case
 
 
-class ReactanceMetric(Metric):
+class ReactanceMetric(RatioScaleMetric):
+    """
+    A metric that measures the presence and strength of Reactance based on a set of test results.
 
-    def __init__(self):
-        pass
+    Attributes:
+        test_results (list[tuple[TestCase, DecisionResult]]): The list of test results to be used for the metric calculation.
+    """
 
-    def _compute(self, test_result: tuple[TestCase, DecisionResult]) -> float:
-        # Extract the decision result from the tuple
-        decision_result: DecisionResult = test_result[1]
-
-        # Extract the control and treatment decisions
-        control_decision = decision_result.CONTROL_DECISION
-        treatment_decision = decision_result.TREATMENT_DECISION
-
-        # Get the number of available options in control and treatment
-        SCALE_OPTIONS_CONTROL = len(decision_result.CONTROL_OPTIONS)
-        SCALE_OPTIONS_TREATMENT = len(decision_result.TREATMENT_OPTIONS)
-
-        # Calculate biasedness as the deviation between the control and treatment decision normalized by the maximum possible deviation
-        if treatment_decision > control_decision:
-            if SCALE_OPTIONS_TREATMENT - 1 - control_decision == 0:
-                # Catch division by zero errors
-                biasedness = 0.0
-            else:
-                biasedness = -1 * (treatment_decision - control_decision) / (SCALE_OPTIONS_TREATMENT - 1 - control_decision)
-        else:
-            if control_decision == 0:
-                # Catch division by zero errors
-                biasedness = 0.0
-            else:
-                biasedness = (control_decision - treatment_decision) / control_decision
-
-        return biasedness
-
-    def compute(self, test_results: list[tuple[TestCase, DecisionResult]]) -> float:
-        # Calculate the average biasedness score across all tests
-        biasedness_scores = [self._compute(test_result) for test_result in test_results]
-        return np.mean(biasedness_scores)
+    def __init__(self, test_results: list[tuple[TestCase, DecisionResult]]):
+        super().__init__(test_results, k=np.array([1]))
