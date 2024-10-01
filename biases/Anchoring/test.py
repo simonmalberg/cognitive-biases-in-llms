b01d154200be7c70afd7b606d3218a3a2e771f74
Roman Poletukhin <poletukhin.rs@phystech.edu>
2024-10-01 02:27:10 +0200

    Anchoring finalised

--- biases/Anchoring/test.py
+++ biases/Anchoring/test.py
@@ -1,6 +1,5 @@
-from base import TestGenerator, LLM, Metric, MetricCalculationError, RatioScaleMetric
+from base import TestGenerator, LLM, RatioScaleMetric
 from tests import TestCase, Template, TestConfig, DecisionResult
-import re
 import numpy as np
 
 
@@ -16,56 +15,50 @@ class AnchoringTestGenerator(TestGenerator):
     def __init__(self):
         self.BIAS: str = "Anchoring"
         self.config: TestConfig = super().load_config(self.BIAS)
+        
+    def sample_custom_values(self, num_instances: int, iteration_seed: int) -> dict:
+        """
+        Sample custom values for the test case generation.
 
-    def generate_all(
-        self, model: LLM, scenarios: list[str], seed: int = 42
-    ) -> list[TestCase]:
-        # Load the custom values from the test configuration
+        Args:
+            num_instances (int): The number of instances expected to be generated for each scenario.
+            iteration_seed (int): The seed to use for sampling the custom values.
+
+        Returns:
+            dict: A dictionary containing the sampled custom values.
+        """
+        sampled_values = {}
+        np.random.seed(iteration_seed)
+        # load the custom values for this test
         custom_values = self.config.get_custom_values()
-
-        # Create test cases for all scenarios
-        test_cases: list[TestCase] = []
-        for scenario in scenarios:
-            try:
-                test_case = self.generate(model, scenario, custom_values, seed)
-                test_cases.append(test_case)
-            except Exception as e:
-                print(
-                    f"Generating the test case failed.\nScenario: {scenario}\nSeed: {seed}"
+        # randomly sample each custom value 'num_instances' number of times
+        # in this case, we are sampling the anchor value
+        for key, value in custom_values.items():
+            if key == "anchor":
+                sampled_values[key] = getattr(np.random, value[0])(
+                    int(value[1]), int(value[2]), size=num_instances
                 )
-                print(e)
-
-        return test_cases
+        return sampled_values
 
     def generate(
-        self, model: LLM, scenario: str, custom_values: dict = {}, seed: int = 42
+        self,
+        model: LLM,
+        scenario: str,
+        custom_values: dict = {},
+        temperature: float = 0.0,
+        seed: int = 42,
     ) -> TestCase:
         # Load the control and treatment templates
         control: Template = self.config.get_control_template()
         treatment: Template = self.config.get_treatment_template()
 
-        # Populate the treatment template with the anchor
-        np.random.seed(seed)
-        # Loading the mean and max interval for the sample of numerical value
-        anchor_min, anchor_max = int(custom_values["anchor"][1]), int(
-            custom_values["anchor"][2]
-        )
-        # Loading the required distribution (should be a np.random method)
-        distribution = getattr(np.random, custom_values["anchor"][0])
-        # Sampling a numerical value
-        anchor = str(
-            int(
-                distribution(
-                    anchor_min,
-                    anchor_max,
-                )
-            )
-        )
         # Inserting the sample into the treatment template
-        treatment.insert("anchor", anchor, origin="user")
-
+        treatment.insert("anchor", str(int(custom_values['anchor'])), origin="user")
+        
         # Populate the templates using the model and the scenario
-        control, treatment = super().populate(model, control, treatment, scenario)
+        control, treatment = super().populate(
+            model, control, treatment, scenario, temperature, seed
+        )
 
         # Create a test case object
         test_case = TestCase(
@@ -73,6 +66,8 @@ class AnchoringTestGenerator(TestGenerator):
             control=control,
             treatment=treatment,
             generator=model.NAME,
+            temperature=temperature,
+            seed=seed,
             scenario=scenario,
             variant=None,
             remarks=None,
@@ -86,12 +81,11 @@ class AnchoringMetric(RatioScaleMetric):
     A class that describes the quantitative evaluation of the anchoring in a model.
     
     Metric:
-    𝔅 = (‖ â₁ − a' ‖₁ − ‖ â₂ − a' ‖₁) / a ∈ [-1, 1];
+    𝔅 = (‖ â₁ − a' ‖₁ − ‖ â₂ − a' ‖₁) / max[‖ â₁ − a' ‖₁, ‖ â₂ − a' ‖₁] ∈ [-1, 1];
 
     where:
     â₁, â₂ are the chosen answers for the control and treatment versions, respectively;
     a' is the answer option closest to the anchor value;
-    a = max[‖ â₁ − a' ‖₁, ‖ â₂ − a' ‖₁].
     """
     
     def __init__(self, test_results: list[tuple[TestCase, DecisionResult]]):
