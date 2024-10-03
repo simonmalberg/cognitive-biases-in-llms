9a8841fba71eb23956e593851ea52bc84df9ccd8
simaex.unknown <simaex.unknown@googlemail.com>
2024-10-03 21:54:40 +0000

    Revisited the test for Information Bias

--- biases/InformationBias/test.py
+++ biases/InformationBias/test.py
@@ -14,79 +14,13 @@ class InformationBiasTestGenerator(TestGenerator):
     """
 
     def __init__(self):
-        self.BIAS = "InformationBias"
+        self.BIAS = "Information Bias"
         self.config = super().load_config(self.BIAS)
 
-    def sample_custom_values(self, num_instances: int, iteration_seed: int) -> dict:
-        """
-        Sample custom values for the test case generation.
-
-        Args:
-            num_instances (int): The number of instances expected to be generated for each scenario.
-            iteration_seed (int): The seed to use for sampling the custom values.
-
-        Returns:
-            dict: A dictionary containing the sampled custom values.
-        """
-        np.random.seed(iteration_seed)
-
-        # initialize dictionary to store sampled values
-        sampled_values = {"prior":[], "posterior_high":[], "control_posterior_low":[], "treatment_posterior_low":[]}
-
-        # load the custom values for this test
-        custom_values = self.config.get_custom_values()
-
-        # randomly sample each custom value 'num_instances' number of times
-        for _ in range(num_instances):
-
-            # Sample prior confidence in course of action
-            min, max, step = custom_values["prior"]
-            prior = random.choice(np.arange(int(min), int(max)+1, int(step)))
-            sampled_values["prior"].append(prior)
-
-            # Sample posteriors lower and higher than prior
-            min, max, step = custom_values["posterior_high"]
-            posterior_high = random.choice(np.arange(int(prior)+5, int(max)+1, int(step)))
-            sampled_values["posterior_high"].append(posterior_high)
-
-            min, max, step = custom_values["control_posterior_low"]
-            control_posterior_low = random.choice(np.arange(int(min), int(max)+1, int(step)))
-            sampled_values["control_posterior_low"].append(control_posterior_low)
-
-            min, max, step = custom_values["treatment_posterior_low"]
-            treatment_posterior_low = random.choice(np.arange(int(min), int(prior), int(step)))
-            sampled_values["treatment_posterior_low"].append(treatment_posterior_low)
-
-        return sampled_values
-
-    def generate(
-        self,
-        model: LLM,
-        scenario: str,
-        custom_values: dict = {},
-        temperature: float = 0.0,
-        seed: int = 42,
-    ) -> TestCase:
-
+    def generate(self, model: LLM, scenario: str, custom_values: dict = {}, temperature: float = 0.0, seed: int = 42) -> TestCase:
         # Load the control and treatment template
         control = self.config.get_control_template()
         treatment = self.config.get_treatment_template()
-
-         # Retrieve and format sampled custom values
-        prior = str(custom_values["prior"])+ "%"
-        posterior_high = str(custom_values["posterior_high"])+ "%"
-        control_posterior_low = str(custom_values["control_posterior_low"])+ "%"
-        treatment_posterior_low = str(custom_values["treatment_posterior_low"])+ "%"
-
-        # Insert the sampled values into the control template
-        control.insert('prior', prior, origin='user')
-        control.insert('posterior_high', posterior_high, origin='user')
-        control.insert('control_posterior_low', control_posterior_low, origin='user')
-
-        # Insert the sampled values into the treatment template
-        treatment.insert('prior', prior, origin='user')
-        treatment.insert('posterior_high', posterior_high, origin='user')
-        treatment.insert('treatment_posterior_low', treatment_posterior_low, origin='user')
 
         # Populate the templates using the model and the scenario
         control, treatment = super().populate(model, control, treatment, scenario)
@@ -108,29 +42,12 @@ class InformationBiasTestGenerator(TestGenerator):
 
 
 class InformationBiasMetric(RatioScaleMetric):
-
     """
-    A class that describes the quantitative evaluation of the Information Bias in a model.
-
-    Metric:
-    𝔅(â₁, â₂) = (â₁ - â₂) / max(â₁, â₂) ∈ [-1, 1]
-
-    where:
-    â₁, â₂ are the chosen answers for the control and treatment versions, respectively (control is shifted by 1: â₁ := â₁ + 1).;
+    A metric that measures the presence and strength of the Information Bias based on a set of test results.
 
     Attributes:
-        test_results (list[tuple[TestCase, DecisionResult]]): A list of test results to be used for the metric calculation.
+        test_results (list[tuple[TestCase, DecisionResult]]): The list of test results to be used for the metric calculation.
     """
+
     def __init__(self, test_results: list[tuple[TestCase, DecisionResult]]):
-
-        max_option = len(test_results[0][1].CONTROL_OPTIONS)
-        
-        for idx, _ in enumerate(test_results):
-
-            test_results[idx][1].CONTROL_DECISION = max_option - test_results[idx][1].CONTROL_DECISION
-            test_results[idx][1].TREATMENT_DECISION = max_option - test_results[idx][1].TREATMENT_DECISION
-            test_results[idx][1].TREATMENT_DECISION -= 1
-            
-        super().__init__(test_results)
-        self.k = 1
-        
+        super().__init__(test_results, flip_treatment=True)
