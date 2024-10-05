0d50c3415571f9448addc50a74502ac488843c42
Roman Poletukhin <poletukhin.rs@phystech.edu>
2024-10-05 02:45:41 +0200

    stable decision script added

--- decision_script.py
+++ decision_script.py
@@ -71,7 +71,7 @@ def decide_batch(
     shuffle_answer_options: bool,
     temperature: float,
     seed: int,
-) -> pd.DataFrame:
+) -> None:
     """
     Decides the dataset batch using the specified model.
 
@@ -84,7 +84,7 @@ def decide_batch(
     seed (int): the seed to use
 
     Returns:
-    pd.DataFrame: the DataFrame representation of the decisions for the batch
+    None
     """
     # initializing the model
     model = get_model(model_name, randomly_flip_options=randomly_flip_options, shuffle_answer_options=shuffle_answer_options)
@@ -96,8 +96,8 @@ def decide_batch(
     for bias in biases:
         test_cases, ids = [], []
         # constructing test cases for all relevant rows in the batch
-        for i, row in batch[batch['bias'].str.strip().str.title().str.replace(' ', '') == bias].iterrows():
-            ids.append(i)
+        for _, row in batch[batch['bias'].str.strip().str.title().str.replace(' ', '') == bias].iterrows():
+            ids.append(row['id'])
             test_cases.append(
                 TestCase(
                     bias=row["bias"],
@@ -113,6 +113,11 @@ def decide_batch(
             )
         # deciding the test cases and obtaining the DecisionResult objects
         decision_results = model.decide_all(test_cases, temperature, seed, max_retries=5)
+        # get indices of failed decisions
+        failed_decisions = [i for i, decision_result in enumerate(decision_results) if decision_result is None]
+        # remove failed decisions from the test cases and ids
+        test_cases = [test_case for i, test_case in enumerate(test_cases) if i not in failed_decisions]
+        ids = [id for i, id in enumerate(ids) if i not in failed_decisions]
         # removing potential failed decisions (None values) from the decision results
         decision_results = [decision_result for decision_result in decision_results if decision_result is not None]
         # storing the results in a new DataFrame
@@ -138,14 +143,18 @@ def decide_batch(
             if decision_batch is None
             else pd.concat([decision_batch, decision_df], ignore_index=True)
         )
-
-    return decision_batch
+    
+    # saving the decisions for the batch to a CSV file with a unique name based on the process ID and timestamp
+    decision_batch.to_csv(
+        f"decision_datasets/{model_name}/batch_{os.getpid()}_decided_{datetime.datetime.now()}.csv",
+        index=False,
+    )
 
 
 def decide_dataset(
     batches: list[pd.DataFrame],
     model_name: str,
-    num_processors: int,
+    num_workers: int,
     randomly_flip_options: bool,
     shuffle_answer_options: bool,
     temperature: float,
@@ -155,19 +164,21 @@ def decide_dataset(
     Function that encapsulates the parallel decision making process for a dataset.
 
     Args:
-    batches (list[pd.DataFrame]): the batches of the dataset to decide (of length num_processors)
+    batches (list[pd.DataFrame]): the batches of the dataset to decide (of length num_workers)
     model_name (str): the name of the model to use
-    num_processors (int): the number of processors used
+    num_workers (int): the number of workers used
     randomly_flip_options (bool): whether to reverse the answer options
     shuffle_answer_options (bool): whether to shuffle the answer options
     temperature (float): the temperature to use
     seed (int): the seed to use
     """
-    results = []
+    # Prepare the directory to store the overall decision dataset for the model
+    os.makedirs(f"decision_datasets/{model_name}", exist_ok=True)
+    
     with tqdm(total=len(batches)) as progress_bar:
-        with concurrent.futures.ProcessPoolExecutor(num_processors) as executer:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=num_workers) as executer:
             # Decide the dataset in parallel
-            for result in executer.map(
+            for _ in executer.map(
                 partial(
                     decide_batch,
                     model_name=model_name,
@@ -178,31 +189,21 @@ def decide_dataset(
                 ),
                 batches,
             ):
-                results.append(result)
                 progress_bar.update()
-    # Concatenate the results
-    decision_dataset = pd.concat(results)
-    # Prepare the directory to store the overall decision dataset for the model
-    os.makedirs(f"decision_datasets", exist_ok=True)
-    # Write the dataset to a CSV file
-    decision_dataset.to_csv(
-        f"decision_datasets/dataset_decided_{model_name}_{datetime.datetime.now()}.csv",
-        index=False,
-    )
 
 
 if __name__ == "__main__":
 
     # TODO: name of the decision model as per the get_model function
-    model_name = "GPT-3.5-Turbo"
+    model_name = "GPT-4o-Mini"
     # TODO: Decide the number of batches to split the dataset into:
-    N_BATCHES = 10
+    N_BATCHES = 3000 # 10 tests per batch
+    # TODO: Number of processors to use
+    max_workers = 100
 
     # Provide the path to the overall dataset if location is different from the default
-    dataset = pd.read_csv("datasets/dataset.csv")
-    # Number of processors to use
-    processors = os.cpu_count()
-    print(f"Number of processors used: {processors}")
+    dataset = pd.read_csv("datasets/full_dataset.csv")
+    print(f"Number of workers used: {max_workers}")
     # Preparing the batches
     batches = np.array_split(dataset, N_BATCHES)
     # Deciding the dataset
@@ -211,7 +212,7 @@ if __name__ == "__main__":
     _ = decide_dataset(
         batches=batches,
         model_name=model_name,
-        num_processors=processors,
+        num_workers=max_workers,
         randomly_flip_options=True,
         shuffle_answer_options=False,
         temperature=0.0,
